af3628e9f6f763d5a3f56dbbc7d82ebe6dec25f5
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-18 15:54:34 -0700

    Completed signup route, added a testing route

--- server/auth/routes.js
+++ server/auth/routes.js
@@ -3,17 +3,29 @@
 const express = require('express');
 const router = express.Router();
 
-const { users } = require('./models')
+const { users } = require('./models');
 
 router.post('/signup', async (req, res, next) => {
-  let userRecord = await users.create(req.body);
-  console.log('signup route hit');
-  res.status(200).json(userRecord); // using .json instead of .send
+  try {
+    let userRecord = await users.create(req.body);
+    console.log('signup route hit');
+    res.status(200).json(userRecord); // using .json instead of .send
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+
 });
 
 router.post('/signin', (req, res, next) => {
   console.log('signin route hit');
   res.status(200).send('Signin Mission Accomplished!');
+});
+
+router.get('/users', async (req, res, next) => {
+  let userTable = await users.findAll({});
+  console.log('users route hit');
+  res.status(200).json(userTable);
 });
 
 module.exports = router;
