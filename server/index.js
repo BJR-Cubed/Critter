b6c4de49e7ebf9cc7bae46c8179c57ef231141a2
Rey Mercado <reymercado.usa@gmail.com>
2022-04-18 11:27:44 -0700

    switch over from rey as driver to brady

--- server/index.js
+++ server/index.js
@@ -12,9 +12,16 @@ const cors = require ('cors');
 app.use(cors());
 app.use(express.json());
 
+app.use('/', (req, res, next) => {
+  console.log('route hit');
+  res.status(204).send('204 code means no content');
+});
+
 app.use('*', (req, res, next) => {
 
-  let error404 = new Error(`cannot ${req.method}`);
+  let error404 = new Error(`Guys, cannot ${req.method} at ${req.route}.`);
+  error404.status = 404;
+  next(error404);
 
 });
 
