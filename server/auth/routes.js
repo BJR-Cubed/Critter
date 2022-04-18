57505199e744c335ea65f50f768272111d00c580
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-18 15:18:47 -0700

    signup route now creates a user record

--- server/auth/routes.js
+++ server/auth/routes.js
@@ -3,10 +3,12 @@
 const express = require('express');
 const router = express.Router();
 
+const { users } = require('./models')
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup', async (req, res, next) => {
+  let userRecord = await users.create(req.body);
   console.log('signup route hit');
-  res.status(200).send('Signup Mission Accomplished!');
+  res.status(200).send(userRecord);
 });
 
 router.post('/signin', (req, res, next) => {
