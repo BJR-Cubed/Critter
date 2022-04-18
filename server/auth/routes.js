25645c24629bf2864c3228028f23505425e1eeab
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-18 15:21:56 -0700

    Changed .send() to .json()

--- server/auth/routes.js
+++ server/auth/routes.js
@@ -8,7 +8,7 @@ const { users } = require('./models')
 router.post('/signup', async (req, res, next) => {
   let userRecord = await users.create(req.body);
   console.log('signup route hit');
-  res.status(200).send(userRecord);
+  res.status(200).json(userRecord); // using .json instead of .send
 });
 
 router.post('/signin', (req, res, next) => {
