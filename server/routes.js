e08a12387f74308595830d70d7167bed7ceb85df
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-19 15:36:35 -0700

    Created POST route for messages

--- server/routes.js
+++ server/routes.js
@@ -2,12 +2,24 @@
 
 const express = require('express');
 const router = express.Router();
+const { messages } = require('./models');
 
 const bearerAuth = require('./auth/middleware/bearer.js');
 
-router.post('/messages', async (req, res, next) => {
+router.post('/messages', bearerAuth, async (req, res, next) => {
   try {
-    res.status(201).send('i am content');
+    if(!req.body) throw new Error('No req.body');
+    // console.log('Type of messages', typeof(messages));
+    console.log(req.body);
+    console.log(req.user);
+    let newRecord = await messages.create({ // .create(req.body)
+      ...req.body,
+      timestamp: Date.now(),
+      length: req.body.body.length,
+      author: req.user.handle,
+    });
+
+    res.status(201).json(newRecord);
     console.log('proof of life');
   } catch (error) {
     console.error(error);
@@ -15,5 +27,11 @@ router.post('/messages', async (req, res, next) => {
   }
 
 });
+
+// Create our routes here
+
+// and have a huge mess of code to create a timestamp, author, calculate the length, etc
+
+
 
 module.exports = router;
