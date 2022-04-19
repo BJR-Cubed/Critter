8676c96fc4714c1aad6df7245c5cb9fd9863dbf2
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-19 15:35:41 -0700

    Added contentSequelize.sync()

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 'use strict';
 
 const { authSequelize } = require('./auth/models');
+const { contentSequelize } = require('./models');
 const { start } = require('./server');
 const PORT = process.env.PORT || 3000;
 
 authSequelize.sync()
   .then(() => {
     console.log('Auth DB is synced');
+  })
+  .catch(console.error);
+
+contentSequelize.sync()
+  .then(() => {
+    console.log('Content DB is synced');
     start(PORT);
   })
   .catch(console.error);
