6550cd3a50ea4b8fab9b76d796b371b07fd97053
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-20 11:22:16 -0700

    Renamed to access.js, added role checking logic

---
+++ server/auth/middleware/access.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Import message model
+const messages = require('../models');
+
+module.exports = async (req, res, next) => {
+  try {
+    const foundMessage = await messages.findOne({id: req.params.id});
+  
+    if(!foundMessage) throw new Error('Message not found');
+    
+    // req.user has all the user info from bearerAuth middleware
+    // Including the user's verified role and handle
+    if(req.user.handle === foundMessage.author || req.user.role === 'admin') {
+      next();
+    } else {
+      // throw an error or something like that
+      let error = new Error('You cannot modify another user\'s content');
+      error.status = 403;
+      next(error);
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+
+};
