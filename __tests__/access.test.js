7080e1e789a2a9123988949f4df84a3b82eb2c11
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-20 14:45:29 -0700

    Added test for deleting another user's message

--- __tests__/access.test.js
+++ __tests__/access.test.js
@@ -1,12 +1,9 @@
 'use strict';
 
 const supertest = require('supertest');
-// const { authSequelize } = require('../server/auth/models/index.js');
-// const { contentSequelize } = require('../server/models/');
 const { app } = require('../server/server.js');
 const request = supertest(app);
 
-//server/index.js needs some work?
 const user1Credentials = {
   handle: 'user1',
   displayName: 'user1',
@@ -19,23 +16,21 @@ const user2Credentials = {
   password: 'password',
 };
 
+const adminCredentials = {
+  handle: 'admin',
+  displayName: 'I am an Admin, fear me',
+  password: 'password',
+  role: 'admin',
+};
+
 let testUser1;
 let testUser2;
-
-// beforeAll((done) => {
-//   authSequelize.sync();
-//   contentSequelize.sync();
-//   done();
-// });
-
-// afterAll(() => {
-//   authSequelize.drop();
-//   contentSequelize.drop();
-// });
+let testAdmin;
 
 describe('Testing message access', () => {
 
   test('User should not be able to update another user\'s message', async () => {
+    // Sign up our users
     testUser1 = await request.post('/signup').send(user1Credentials);
     testUser1 = testUser1.body;
 
@@ -48,13 +43,42 @@ describe('Testing message access', () => {
       .send({ body: 'I love having secure messages' });
 
     let id = response.body.id;
-    
+
     response = await request
       .put(`/messages/${id}`)
       .set('Authorization', `Bearer ${testUser2.token}`)
       .send({ body: 'I hate having secure messages' });
 
     expect(response.status).toBe(403);
+  });
+
+  test('User should not be able to delete another user\'s message', async () => {
+    let response = await request
+      .post('/messages')
+      .set('Authorization', `Bearer ${testUser1.token}`)
+      .send({ body: 'I own my own messages' });
+
+    let id = response.body.id;
+
+    response = await request
+      .delete(`/messages/${id}`)
+      .set('Authorization', `Bearer ${testUser2.token}`);
+
+    expect(response.status).toBe(403);
+  });
+
+  test('Admin should be able to update another user\'s message', async () => {
+    // signup the admin
+    testAdmin = await request.post('/signup').send(adminCredentials);
+    testAdmin = testAdmin.body;
+
+    // post a message
+
+    // try to update it
+  });
+
+  test('Admin should be able to delete another user\'s message',{
+
   });
 
   // Test for admin bypass
