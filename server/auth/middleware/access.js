6e478a4be7a847c45f58382e7dd918cc7e2b2618
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-20 12:18:47 -0700

    Fixed message model import

--- server/auth/middleware/access.js
+++ server/auth/middleware/access.js
@@ -1,17 +1,17 @@
 'use strict';
 
 // Import message model
-const messages = require('../models');
+const { messages } = require('../../models');
 
 module.exports = async (req, res, next) => {
   try {
-    const foundMessage = await messages.findOne({id: req.params.id});
-  
-    if(!foundMessage) throw new Error('Message not found');
-    
+    const foundMessage = await messages.findOne({ where: { id: req.params.id } });
+
+    if (!foundMessage) throw new Error('Message not found');
+
     // req.user has all the user info from bearerAuth middleware
     // Including the user's verified role and handle
-    if(req.user.handle === foundMessage.author || req.user.role === 'admin') {
+    if (req.user.handle === foundMessage.author || req.user.role === 'admin') {
       next();
     } else {
       // throw an error or something like that
