d057d959ada13bb8a566581db248d1c2d488857e
Rey Mercado <reymercado.usa@gmail.com>
2022-04-21 14:30:26 -0700

    proof of login

--- client/index.js
+++ client/index.js
@@ -50,7 +50,26 @@ const questions = [
       return answers.enterNewHandle;
     },
   },
-  
+  {
+    type: 'input',
+    name: 'enterLoginHandle',
+    message: 'Please enter your Login Handle:',
+    prefix: '',
+    when(answers) {
+      return answers.hasAccount === 'yes';
+    } ,
+  },
+  {
+    type: 'password',
+    name: 'enterPassword',
+    message: 'Please enter your password:',
+    mask: '*',
+    prefix: '',
+    when(answers) {
+      return answers.hasAccount === 'yes';
+    } ,
+  },
+
 ];
 
 let handleSignup = async (displayName, handle, password) =>{
@@ -60,7 +79,18 @@ let handleSignup = async (displayName, handle, password) =>{
     password,
   };
   let response = await axios.post(`${API_URL}/signup`, data );
-  console.log(response.data);
+  console.log('handlesignup response.data:', response.data);
+};
+
+//signin function needed
+let handleSignin = async (handle, password) =>{
+  // let data = {
+  //   handle,
+  //   password,
+  // };
+  console.log('handle and password are:', handle, password);
+  let response = await axios.post(`${API_URL}/signin`, null, {auth: {username: handle, password: password} } );
+  // console.log('handlesignin response.data:', response.data);
 };
 
 
@@ -69,11 +99,21 @@ inquirer
   .then( async (answers) => {
     console.log(answers, ' are the answers');
     globalAnswers[0] = answers;
-    await handleSignup(answers.enterNewDisplayName, answers.enterNewHandle, answers.newPassword);
+    if (answers.hasAccount === 'yes') {
+      // handleSignin function called here
+      await handleSignin(answers.enterLoginHandle, answers.enterPassword);
+    } else if (answers.hasAccount === 'no') {
+      await handleSignup(answers.enterNewDisplayName, answers.enterNewHandle, answers.newPassword);
+    }
   })
   .then(() => {
-    console.log(globalAnswers[0].newPassword);
+    console.log('globalanswers[]0.newpassword is', globalAnswers[0].newPassword);
   });
+
+
+// signin sign up block
+// while loop inquirer prompt until escape key
+
 
 // inquirer.prompt(questions).then((answers) => {
 //   console.log('\nOrder receipt:');
